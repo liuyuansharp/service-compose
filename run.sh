bd5741b8439c632bf8aa48161d5b0591eb5daf51
liuyuan <liuyuansharp@foxmail.com>
2026-02-09 23:05:54 +0800

    fix

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 
 source venv/bin/activate
 
-nohup sh ./test_example.sh &
-
+nohup python3 manage_services.py start --config examples/services_config_example.json --daemon &
 nohup python3 dashboard_api.py --host 0.0.0.0 --port 8080 &
